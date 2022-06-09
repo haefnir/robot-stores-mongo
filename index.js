c286e62411de456c768df4fb4345386dc09cb5c6
Seb Lamberts <seb.lamberts@eyup.com>
2022-06-09 16:27:06 +0100

    lesgo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Schema = require('validate')
 const MongoClient = require('mongodb').MongoClient
 const ObjectId = require('mongodb').ObjectId
 
@@ -9,7 +10,7 @@ const url = "mongodb://root:password@localhost:27017"
 
 const getDb = async () => {
     let connection = {}
-    connection = await MongoClient.connect(url)
+    connection = await MongoClient.connect(url, {ignoreUndefined: true})
     return connection.db('robot-store').collection('products')
 }
 
@@ -28,6 +29,192 @@ const dbMiddleware = async (req, res, next) => {
     next()
 }
 
-app.use(dbMiddleware)
+const dataMiddleware = (req, res, next) => {
+    res.locals.data = {
+        title: req.body.title,
+        price: req.body.price,
+        image: req.body.image,
+        category_id: req.body.category_id,
+        category: req.body.category,
+        character_id: req.body.character_id,
+        character: req.body.character,
+        description: req.body.description,
+        image2: req.body.image2,
+        image3: req.body.image3
+    }
+    next()
+}
+
+const productValidator = new Schema({
+    title : {
+        type : String,
+        length : {min : 5, max : 255}
+    },
+    price : {
+        type : Number,
+        size : {min: 0}
+    },
+    image : {
+        type : String
+    },
+    category_id : {
+        type : Number,
+        size : {min : 1, max : 4}
+    },
+    category : {
+        type : String,
+        enum : ['Aprons', 'Baseball Hats', 'Mugs', 'T-shirts']
+    },
+    character_id : {
+        type : Number,
+        size : {min : 1, max : 4}
+    },
+    character : {
+        type : String,
+        enum : ['Fred', 'Dolores', 'Bubbles', 'Rex', 'Harold']
+    },
+    description : {
+        type : String
+    },
+    image2 : {
+        type : String
+    },
+    image3 : {
+        type : String
+    }
+})
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(dbMiddleware)
+
+
+app.get('/api/products', async (req, res) => {
+    const category = req.query.category
+    const character = req.query.character
+
+    let conditions = []
+    if (category){
+        conditions.push({category})
+    }
+    if (character){
+        conditions.push({character})
+    }
+
+    let final_condition = (conditions.length > 0) ? {$and: conditions} : {};
+
+    const products = await res.locals.connection.find(final_condition).project({_id: true, title: true, price: true, image: true}).toArray()
+
+    res.status(200).json({
+        status: 200,
+        message: 'Data retrieved successfully',
+        data: products
+    })
+})
+
+app.get('/api/products/:id', async (req, res) => {
+    const _id = ObjectId(req.params.id)
+    const product = await res.locals.connection.find({_id}).toArray()
+
+    if(product.id) {
+        res.status(200).json({
+            "status": 200,
+            "message": "Products retrieved successfully!",
+            "data": product
+        })
+    } else {
+        res.status(400).json({
+            "status": 400,
+            "message": "Invalid ID",
+            "data": null
+        })
+    }
+})
+
+app.post('/api/products', dataMiddleware, async (req, res) => {
+    const errors = productValidator.validate(res.locals.data)
+
+    if(errors.length > 0) {
+        let message = ''
+        errors.forEach(error => {
+            message += error.message + ' '
+        })
+        return res.status(400).json({
+            status: 400,
+            message: message,
+            data: null
+        })
+    }
+
+    const result = await res.locals.connection.insertOne(res.locals.data)
+
+    if(result.insertedId) {
+        res.status(200).json({
+            status: 200,
+            message: 'Document added successfully',
+            data: "id: " + result.insertedId
+        })
+    } else {
+        res.status(400).json({
+            status: 400,
+            message: 'Failed to add document',
+            data: null
+        })
+    }
+})
+
+app.put('/api/products/:id', dataMiddleware, async (req, res) => {
+    const _id = ObjectId(req.params.id)
+
+    const errors = productValidator.validate(res.locals.data)
+
+    if(errors.length > 0) {
+        let message = ''
+        errors.forEach(error => {
+            message += error.message + ' '
+        })
+        return res.status(400).json({
+            status: 400,
+            message: message,
+            data: null
+        })
+    }
+
+    const result = await res.locals.connection.updateOne({_id}, {$set: res.locals.data})
+
+    if(result.modifiedCount === 0) {
+        res.status(400).json({
+            status: 400,
+            message: 'Update failed',
+            data: null
+        })
+    } else {
+        res.status(200).json({
+            status: 200,
+            message: 'Updated successfully',
+            data: null
+        })
+    }
+})
+
+app.delete('/api/products/:id', async (req, res) => {
+    const _id = ObjectId(req.params.id)
+
+    const result = await res.locals.connection.deleteOne({_id})
+
+    if(result.deletedCount === 0) {
+        res.status(400).json({
+            status: 400,
+            message: 'Delete failed',
+            data: null
+        })
+    } else {
+        res.status(200).json({
+            status: 200,
+            message: 'Deleted successfully',
+            data: null
+        })
+    }
+})
+
+app.listen(port)
